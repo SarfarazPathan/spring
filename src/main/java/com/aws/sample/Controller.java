cefb21b5894ab7cb57fdddd1cfb07084387fe175
SarfarazPathan <sarfaraz.ip@gmail.com>
2021-04-27 00:02:51 +0530

    new commit

--- src/main/java/com/aws/sample/Controller.java
+++ src/main/java/com/aws/sample/Controller.java
@@ -1,5 +1,6 @@
 package com.aws.sample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class Controller {
 
+	@Value("${aws.db2.password}")
+	private String password;
+	
 	@GetMapping("/getMsg")
 	public String getMsg() {
+		System.out.println("Property values : "+ password);
 		return "Application is UP & RUNNING ...";
 	}
 	
 	@GetMapping("/getMsg/{name}")
 	public String getMsg(@PathVariable String name) {
+		System.out.println("Property values : "+ password);
 		return "Wel Come to world of Microservices ..."+ name;
 	}
 }
